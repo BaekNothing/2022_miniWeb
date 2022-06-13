6c0b3dcb2fc38feca498e9bc95a70409d6b7d27a
baeknothing <baeknothing@gmail.com>
2022-06-13 16:51:07 +0900

    zustand Test

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import create from 'zustand';
+
+const Counter = create((set) => ({
+  number : 0,
+  setNumber : (number) => set({number})
+}))
+
+function NumberCount() {
+  const {number, setNumber} = Counter();
+  return (
+    <div>
+      <h1>{number}</h1>
+      <button onClick={() => setNumber(number + 1)}>+</button>
+      <button onClick={() => setNumber(number - 1)}>-</button>
+    </div>
+  );
+}
+
+function SyncCount() {
+  const {number, setNumber} = Counter();
+  return (
+    <div>
+      <h1>{number}</h1>
+    </div>
+  )
+}
+
+const stringState = create((set) => ({
+  string : "",
+  setString : (string) => set({string})
+}))
+
+function StringState() {
+  
+  const {string, setString} = stringState();
+
+  useEffect(() => {
+    axios.get('http://localhost:8080')
+        .then(function (response) { 
+          console.log(response.data.test); 
+          setString(response.data.test);
+        })
+        .catch(function (error) { console.log(error); })
+  }, [])
+
+  return (
+    <div>
+      <h1>{string}</h1>
+      {/* <input type="text" value={string} onChange={(e) => setString(e.target.value)} /> */}
+    </div>
+  )
+}
 
 function App() {
-    useEffect(() => {
-        axios.get('http://localhost:8080')
-            .then(function (response) { console.log(response.data.test); })
-            .catch(function (error) { console.log(error); })
-    }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <NumberCount></NumberCount>
+        <SyncCount></SyncCount>
+        <StringState></StringState>
         <a
           className="App-link"
           href="https://reactjs.org"
