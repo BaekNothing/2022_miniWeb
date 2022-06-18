1d6da0305f968e2d9f83de2e5a384e83d2af0d3e
BaekNothing <baeknothing@gmail.com>
2022-06-18 20:52:24 +0900

    update buttonLogic

--- client/src/App.js
+++ client/src/App.js
@@ -1,76 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import axios from "axios";
 import React, { useEffect, useState } from 'react';
-import create from 'zustand';
 import PageRender from './PageManager'
-
-const Counter = create((set) => ({
-  number : 0,
-  setNumber : (number) => set({number})
-}))
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <NumberCount></NumberCount>
+        {/* <NumberCount></NumberCount>
         <SyncCount></SyncCount>
-        <StringState></StringState>
+        <StringState></StringState> */}
         <PageRender></PageRender>
       </header>
     </div>
   );
-}
-
-function NumberCount() {
-  const {number, setNumber} = Counter();
-  return (
-    <div>
-      <h1>{number}</h1>
-      <button onClick={() => setNumber(number + 1)}>+</button>
-      <button onClick={() => setNumber(number - 1)}>-</button>
-    </div>
-  );
-}
-
-function SyncCount() {
-  const {number, setNumber} = Counter();
-  return (
-    <div>
-      <h1>{number}</h1>
-    </div>
-  )
-}
-
-const stringState = create((set) => ({
-  string : "",
-  setString : (string) => set({string})
-}))
-
-function StringState() {
-  
-  const {string, setString} = stringState();
-
-  useEffect(() => {
-    axios.get('http://localhost:8080')
-        .then(function (response) {
-          setString(response.data[0]?.name??"noData");
-        })
-        .catch(function (error) { console.log(error); })
-  }, [setString])
-
-  return (
-    <div>
-      <h1>{string}</h1>
-      {/* <input type="text" value={string} onChange={(e) => setString(e.target.value)} /> */}
-    </div>
-  )
 }
 
 export default App;
--- client/src/PageManager.js
+++ client/src/PageManager.js
@@ -15,14 +15,60 @@ const pageIndex = create((set)=>({
     })}
 ))
 
+const userSelectData = create((set)=>({
+    userSelect : [],
+    setUserSelect : (userSelect) => set(() => {
+        return {userSelect} ;
+    })}
+))
+
+var refreshUserSelectData_flag = true;
+var refreshUserSelectData_data = 0;
+function SetUserSelectDataFlag(flag, data) {
+    refreshUserSelectData_data = data;
+    refreshUserSelectData_flag = flag;
+}
+function SetUserSelectData(userSelect, setUserSelect, index, data){
+    useEffect(() => {
+        if(refreshUserSelectData_flag)
+        {
+            var tempAry = [...userSelect];
+            while (tempAry.length <= index)
+                tempAry.push(0);
+            tempAry[index] = refreshUserSelectData_data;
+            console.log(tempAry);
+            setUserSelect(tempAry);
+            refreshUserSelectData_flag = false;
+        }}, [setUserSelect, index, data])
+    
+    const result = [];
+    for (let i = 0; i < userSelect.length; i++) {
+        if(i  !== index)
+            result.push(<p key={i}>{userSelect[i]}</p>);
+        else
+            result.push(<p style={{color:"red"}} key={i}>{userSelect[i]}</p>);
+    }
+    return result;
+}
+
 function PageRender (){
-    const {index, setIndex} = pageIndex();
-    const { dataAry} = pageData();
+    const { dataAry, setAry } = pageData();
+    const { index, setIndex } = pageIndex();
+    const { userSelect, setUserSelect } = userSelectData();
 
     return (
-        <div>
-            <p> {index} </p>
-            <button onClick={()=>{setIndex(index + 1);}}>replace</button>
+        <div key="setUserSelectData">
+            <p> {dataAry[0]} </p>
+            <p> page : {index} </p>
+            <p> {dataAry[1]} </p>
+            <p> {dataAry[2]} </p>
+            <button onClick={() => { setIndex((index > 1 ? index - 1 : 0)); SetUserSelectDataFlag(true, refreshUserSelectData_data) }}>movePrev</button>
+            <input type="radio" name="radio" value={dataAry[3]} onChange={() => { SetUserSelectDataFlag(true, 1); }}/>
+            <input type="radio" name="radio" value={dataAry[4]} onChange={() => { SetUserSelectDataFlag(true, 2); }}/>
+            <input type="radio" name="radio" value={dataAry[5]} onChange={() => { SetUserSelectDataFlag(true, 3); }}/>
+            <input type="radio" name="radio" value={dataAry[6]} onChange={() => { SetUserSelectDataFlag(true, 4); }}/>
+            <button onClick={() => { setIndex(index + 1); SetUserSelectDataFlag(true, refreshUserSelectData_data) }}>moveNext</button>
+            {SetUserSelectData(userSelect, setUserSelect, index, refreshUserSelectData_data)}
             {GetPageData(index)}
             {RenderPage(index)}
         </div>
@@ -75,21 +121,19 @@ function RenderPage(){
             tempAry[4] = pageData_from_server.data[index]?.a2 ?? "noData";
             tempAry[5] = pageData_from_server.data[index]?.a3 ?? "noData";
             tempAry[6] = pageData_from_server.data[index]?.a4 ?? "noData";
-            console.log(pageData_from_server[index]);
             setAry(tempAry);
         }
     }, [setAry, index])
 
-    return (
-        <div>
-            <p> {dataAry[0]}, </p>
-            <p> {dataAry[1]}, </p>
-            <p> {dataAry[2]}, </p>
-            <p> {dataAry[3]}, </p>
-            <p> {dataAry[4]}, </p>
-            <p> {dataAry[5]}, </p>
-            <p> {dataAry[6]}, </p>
-        </div>)
+    return ( <div>{returnArrayToP(dataAry)} </div>)
+}
+
+function returnArrayToP(ary){
+    var result = [];
+    for (let i = 0; i < ary.length; i++) {
+        result.push(<p key={i}>{ary[i]}</p>);
+    }
+    return result;
 }
 
 export default PageRender;
