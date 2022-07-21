415c482798918d715353b1db2eb73222c1345087
BaekNothing <baeknothing@gmail.com>
2022-07-22 00:07:33 +0900

    saperateServerData

--- server/Router/test.js
+++ server/Router/test.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const csvParser = require('csv-parser');
 const result = [];
 
-router.get('/', async (req, res) => {
+router.get('/selectData', async (req, res) => {
     if(result.length === 0){
             result.length = 0;
         await fs.createReadStream("./Router/data/data.csv")
@@ -15,6 +15,22 @@ router.get('/', async (req, res) => {
         .on("end", () => {
             res.send(result);
         });
+    }
+    else
+        res.send(result);
+});
+
+router.get('/resultData', async (req, res) => {
+    if (result.length === 0) {
+        result.length = 0;
+        await fs.createReadStream("./Router/data/data.csv")
+            .pipe(csvParser())
+            .on("data", (data) => {
+                result.push(data);
+            })
+            .on("end", () => {
+                res.send(result);
+            });
     }
     else
         res.send(result);
