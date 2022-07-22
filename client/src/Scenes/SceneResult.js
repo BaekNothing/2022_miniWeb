55c84732ac459654732624fdbda4c5ba9b24bf22
baeknothing <baeknothing@gmail.com>
2022-07-22 11:24:42 +0900

    Add SendDataSys

--- client/src/Scenes/SceneResult.js
+++ client/src/Scenes/SceneResult.js
@@ -1,21 +1,23 @@
 import '../App.css';
+import axios from "axios";
 import React, { useEffect } from 'react';
 import { sceneData, pageData, userSelectData } from '../Data/AppVars';
 import RenderLineChart from '../Data/ChartDrawer';
 
 function RenderResultPage(prop) {
     const { setPageIndex} = pageData();
-    const { setUserSelect } = userSelectData();
+    const { userSelect, setUserSelect } = userSelectData();
     const { setSceneIndex } = sceneData();
 
     useEffect(() => {
-
+        
     }, [])
 
     return (
         <div className="main-body" key="introPage">
             <p>this_Is_result_Page</p>
             <button onClick={() => {
+                sendData(userSelect)
                 setSceneIndex(0);
                 clearAlldata(setPageIndex, setUserSelect);
             }}> press to GoHome</button>
@@ -28,6 +30,22 @@ function RenderResultPage(prop) {
 }
 
 // *************** ACTION ***************
+
+function sendData(userSelect){    
+    axios({
+        method: 'post',
+        url: 'http://localhost:8080/SetResultData',
+        params: {
+            email: "baeknothing@gmail.com",
+            name: "baeknothing",
+            answer: userSelect
+        }
+    })
+        .then(function async(response) {
+            console.log(response);
+        })
+        .catch(function (error) { console.log(error); })
+}
 
 function clearAlldata(setPageIndex, setUserSelect) {
     setPageIndex(0);
---
+++ server/Router/data.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const csvParser = require('csv-parser');
+const validator = require("validator");
+const result = [];
+
+router.post('/GetSelectData', async (req, res) => {
+    if(result.length === 0){
+            result.length = 0;
+        await fs.createReadStream("./Router/data/data.csv")
+        .pipe(csvParser())
+        .on("data", (data) => {
+            result.push(data);
+        })
+        .on("end", () => {
+            res.send(result);
+        });
+    }
+    else
+        res.send(result);
+});
+
+router.post('/GetResultData', async (req, res) => {
+    
+})
+
+router.post('/SetResultData', async (req, res) => {
+    var email = req.query.email;
+    var name = req.query.name;
+    var answer = req.query.answer;
+
+    var result = [email, name];
+    answer.forEach(child => {
+        result += "," + child;
+    });
+    console.log(result);
+
+    if(validator.isEmail(email))
+    {
+        await WriteResultData(result);
+        res.send("success");
+    }
+    else 
+       res.send("fail");
+});
+
+async function WriteResultData(result) {
+    const file = "./Router/data/result.csv";
+    result;
+    
+    await fs.open(file,'a',function(err,fd){
+        if (err) throw err;
+         console.log(fd,'file open complete');}
+    );
+
+    await fs.appendFile(
+        "./Router/data/result.csv", 
+        `${result}\n`,
+        function(err, fd){
+            if (err) throw err;
+            console.log(fd, 'file append complete');
+        }
+    )
+};
+
+module.exports = router;
