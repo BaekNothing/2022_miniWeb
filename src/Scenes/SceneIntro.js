362903e17a12ec931fbf707cd5e8314165bb98e5
BaekNothing <baeknothing@gmail.com>
2023-03-24 00:32:44 +0900

    update IntroPage

--- src/Scenes/SceneIntro.js
+++ src/Scenes/SceneIntro.js
@@ -44,7 +44,7 @@ function RenderIntroPage(prop) {
                 <ReCAPTCHA
                 style={{
                     position: "absolute",
-                    top: '500px',
+                    top: '480px',
                     left: "50%",
                     transform: "translateX(-50%)",
                 }}
@@ -59,8 +59,17 @@ function RenderIntroPage(prop) {
     {
         return (
             <div className="main-body" key="introPage">
-                <img key='introTitle' className='title_image' id='char_body' src={'./images/intro_png.png'} alt='charBody' />
-                <button className='title_btn' onClick={() => {
+                {/* <img key='introTitle' style={{opacity: 0.3}} 
+                className='title_image' id='char_body' src={'./images/intro_png.png'} alt='charBody' />                 */}
+                <DescTitle />
+                <button className='title_btn'
+                style={{
+                    position: "absolute",
+                    left: "50%",
+                    transform: "translateX(-50%)",
+                    top: '568px',
+                }}
+                onClick={() => {
                     _startFlag = 2;
                     SetDummy(1);
                 }}> 내 위치 알아보기</button>
@@ -72,8 +81,25 @@ function RenderIntroPage(prop) {
     {
         return (
             <div className="main-body" key="introPage">
-                <img key='introTitle' className='call_image' id='char_body' src={'./images/call_png.png'} alt='charBody' />
-                <form onSubmit={handleSubmit}>
+                {/* <img key='introTitle' className='call_image' id='char_body' src={'./images/call_png.png'} alt='charBody' /> */}
+                <div
+                    style={{
+                        position: "absolute",
+                        top:'120px',
+                        width: commonWidth,
+                    }}
+                >   어떻게 불러줄까?
+
+                </div>
+                
+                <form onSubmit={handleSubmit}
+                    style={{
+                        position: "absolute",
+                        width: commonWidth,
+                        top: "500px"
+
+                    }}
+                >
                     <input className='title_btn_white' id="name" type="text" name="name" placeholder="5자 이내로 입력해줘" />
                     <input type="submit" value="확인" onClick={() => {
                         const name = document.getElementsByName("name")[0].value;
@@ -165,6 +191,109 @@ function IntroTitleImage(){
                     transform: 'translateX(-50%)',
                 }}
                 src={'./images/title_img.png'} alt='charBody' />
+        </div>
+    )
+}
+
+// *************** DescTitle ***************
+
+function DescTitle(){
+    return (
+        <div>
+            <DescTitleTitle />
+            <DescTitleDesc />
+        </div>
+    )
+}
+
+function DescTitleTitle() {
+    return (
+        <div
+            style={{
+                textAlign: 'center',
+                color: '#2F4FFD'
+            }}
+        > 
+            <p 
+                style={{
+                    position: 'absolute',
+                    top: '20px',
+                    width: commonWidth,
+                    fontSize: '54px',
+            }}
+            >"</p>
+            <div 
+                style={{
+                    position: 'absolute',
+                    top: '120px',
+                    width: commonWidth,
+                    fontSize: '24px',
+                }}
+            > 솔직히, 궁금하잖아?</div>
+            <div
+                style={{
+                    position: 'absolute',
+                    backgroundColor: 'white',
+                    height: '80px',
+                    width: '280px',
+                    borderRadius: '20px',
+                    paddingTop: '24px',
+                    top: '170px',
+                    left: '50%',
+                    transform: 'translateX(-50%)',
+                    fontSize: '14px',
+                    lineHeight: '20px',
+                }}
+            >
+                "인격체를 가볍게 순위 매기고<br/>평가하는 것은 옳지 못한 일일 뿐더러<br/>그 기준도 명확하지 않아"
+            </div>
+        </div>
+    )
+}
+
+function DescTitleDesc() {
+    return (
+        <div
+            style={{
+                fontSize:'14px',
+                lineHeight: '20px',
+            }}  
+        >
+            <div
+                style={{
+                    position: 'absolute',
+                    top: '316px',
+                    fontFamily: 'SUIT-Thin',
+                    width: commonWidth,
+                }}
+            > 
+                응, 너의 말에 동의해 <br />
+            </div>
+            <div
+                style={{
+                    position: 'absolute',
+                    top: '350px',
+                    fontFamily: 'SUIT-Thin',
+                    width: commonWidth,
+                }}
+            >
+                하지만, 우리는 쉽게 평가당하고, 또 순위 매기지, <br />
+                때로는 우리스스로 평가당하기를 원하기도 해 <br />
+                높은 점수를 가지면 우대해주길 바라고 <br />
+                조건이 좋지 않다면 보살펴주길 바라지 <br />
+            </div>
+            <div
+                style={{
+                    position: 'absolute',
+                    top: '450px',
+                    fontFamily: 'SUIT-Thin',
+                    width: commonWidth,
+                }}
+            >
+                그래서, 우리는 이런 테스트를 만들었어 <br />
+                나는 평균보다 우월한가? 평균보다 불우한가? <br />
+                지금의 내 위치는 어디일까? <br/>
+            </div>
         </div>
     )
 }
